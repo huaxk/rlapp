0d1295be424943f0fd237ef44296d0be65ff96e8
huaxk <huaxk@163.com>
2019-06-25 16:10:04 +0800

    add axios shims

--- src/shims-quasar.d.ts
+++ src/shims-quasar.d.ts
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import { AxiosStatic } from "axios";
 
 declare module 'vue/types/vue' {
   interface Vue {
     $q: any;
+    $axios: AxiosStatic;
+    loading: boolean;
   }
 }
